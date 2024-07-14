785d7d14f96daea70755c9aed7cb8c63496e0c6a
Mike M <60558257+Izote@users.noreply.github.com>
2024-07-14 19:05:19 -0400

    added pagination - damn near forgot

--- ao3/get_response.py
+++ ao3/get_response.py
@@ -1,18 +1,24 @@
+from random import randint
+from time import sleep
 from requests import get
 from bs4 import BeautifulSoup
 
 
-def get_response(url: str, features: str = "html.parser") -> BeautifulSoup:
+def get_response(url: str, pause: tuple[int, int] = (3, 6),
+                 features: str = "html.parser") -> BeautifulSoup:
     """
     Perform a GET request on an AO3 page and return a bs4.BeautifulSoup
-    instance for parsing.
+    instance for parsing. Waits a random number of seconds after the request
+    to facilitate sequential calls.
 
     :param url: target URL.
+    :param pause: (min, max) time in seconds between requests.
     :param features: `features` parameter utilized by bs4.BeautifulSoup call.
 
     :return: site content as a bs4.BeautifulSoup instance.
 
     """
+    m, n = pause
     response = get(url)
 
     if response.status_code != 200:
@@ -25,4 +31,5 @@ def get_response(url: str, features: str = "html.parser") -> BeautifulSoup:
     if error_msg is not None:
         raise ValueError(error_msg)
     else:
+        sleep(randint(m, n))
         return BeautifulSoup(response.text, features=features)
--- ao3/get_work.py
+++ ao3/get_work.py
@@ -10,37 +10,52 @@ def get_work(user_name: str) -> list[dict]:
 
     :return: list[dict] where each dict is in {"column": "value"} row format.
     """
-    url = f"https://archiveofourown.org/users/{user_name}/works/"
+    url = f"https://archiveofourown.org/users/{user_name}/works?page=1"
     soup = get_response(url)
 
-    # to do: paginate the process below.
-    blurb = soup.find("ol", {"class": "work index group"})
-    art = blurb.find_all("li", {"role": "article"})
+    navigation = soup.find("ol", {"role": "navigation"})
+    if navigation is not None:
+        end = int(navigation.text.split(" ")[-3])
+    else:
+        end = 1
 
-    var = ["id", "date", "language", "words", "chapters", "comments",
-           "kudos", "bookmarks", "hits"]
-    int_var = ["words", "kudos", "bookmarks", "hits"]
-
-    user_id = list(filter(lambda s: s.find("user") >= 0, art[0]["class"]))[0]
-    user_id = user_id.replace("user-", "")
-
+    uid = None
     row = []
-    for a in art:
-        work = {"user_id": user_id,
-                "id": a["id"].replace("work_", ""),
-                "title": a.find("h4").find("a").text,
-                "date": a.find("p", {"class": "datetime"}).text}
+    for i in range(1, end + 1):
+        if i != 1:
+            url = url.replace(f"page={str(i - 1)}", f"page={str(i)}")
+            soup = get_response(url)
 
-        stat = a.find("dl", {"class": "stats"})
-        for key in var[2:]:
-            value = stat.find("dd", {"class": key})
-            if key in int_var:
-                work[key] = 0 if value is None else int(value.text)
-            else:
-                work[key] = "" if value is None else value.text
+        blurb = soup.find("ol", {"class": "work index group"})
+        art = blurb.find_all("li", {"role": "article"})
 
-        work["date"] = datetime.strptime(work["date"], "%d %b %Y").date()
+        var = ["id", "date", "language", "words", "chapters", "comments",
+               "kudos", "bookmarks", "hits"]
+        int_var = ["words", "comments", "kudos", "bookmarks", "hits"]
 
-        row.append(work)
+        if i == 1:
+            uid = list(filter(lambda s: s.find("user") >= 0, art[0]["class"]))
+            uid = uid[0].replace("user-", "")
+
+        for a in art:
+            work = {"user_id": uid,
+                    "id": a["id"].replace("work_", ""),
+                    "title": a.find("h4").find("a").text,
+                    "date": a.find("p", {"class": "datetime"}).text}
+
+            stat = a.find("dl", {"class": "stats"})
+            for key in var[2:]:
+                value = stat.find("dd", {"class": key})
+                if key in int_var:
+                    if value is None:
+                        work[key] = 0
+                    else:
+                        work[key] = int(value.text.replace(",", ""))
+                else:
+                    work[key] = "" if value is None else value.text
+
+            work["date"] = datetime.strptime(work["date"], "%d %b %Y").date()
+
+            row.append(work)
 
     return row
